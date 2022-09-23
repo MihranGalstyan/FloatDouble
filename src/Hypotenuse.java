59563f9ad4079a2482d64795cdef66c30ecc4db5
Mihran Galstyan <msho.mher@gmail.com>
2022-09-23 17:21:35 +0400

    Initial commit

---
+++ src/Hypotenuse.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Mihran Galstyan
+ * All rights reserved
+ */
+public class Hypotenuse {
+    public static void main(final String[] args) {
+        float sideA = 3.0f;
+        float sideB = 4.0f;
+        double hypotenuse = Math.sqrt(sideA * sideA + sideB * sideB);
+
+        System.out.println("Hypotenuse is " + hypotenuse);
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Mihran Galstyan
+ * All rights reserved
+ */
+public class Square {
+    public static void main(final String[] args) {
+        float r = 11.1f;
+        float pi = 3.14f;
+        double s = pi * r * r;
+
+        System.out.println("Square = " + s);
+    }
+}
